a7f83f7b366e99acb133dd7ef456580c8294e4a9
ajayd4522 <ajaydewangan002@gmail.com>
2023-11-11 00:20:28 +0530

    Edited First

--- First.java
+++ First.java
@@ -1,5 +1,9 @@
-package SuperApp.Super-app;
+package SuperApp.Super_app;
 
 public class First {
-    
+    public static void main(String[] args) {
+        System.out.println("Everything is fine");
+        for(int i=0; i<100; i++) System.out.println("Hello World");
+        
+    }
 }
